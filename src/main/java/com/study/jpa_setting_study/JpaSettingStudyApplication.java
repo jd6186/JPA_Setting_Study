0d283a191cab367213002667cf3f9ba3d21dab49
Donguk Jung <wjdehd7991@gmail.com>
2022-02-27 09:19:14 +0900

    기본 세팅 추가

---
+++ src/main/java/com/study/jpa_setting_study/JpaSettingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.jpa_setting_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSettingStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSettingStudyApplication.class, args);
+	}
+
+}
