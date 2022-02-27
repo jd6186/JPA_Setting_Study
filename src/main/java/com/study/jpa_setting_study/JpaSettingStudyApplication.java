589029936887d3c8b9d588c0654c35fd92217546
Donguk Jung <wjdehd7991@gmail.com>
2022-02-27 16:30:51 +0900

    DB 연결 조건 제거

--- src/main/java/com/study/jpa_setting_study/JpaSettingStudyApplication.java
+++ src/main/java/com/study/jpa_setting_study/JpaSettingStudyApplication.java
@@ -1,11 +1,16 @@
 package com.study.jpa_setting_study;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class JpaSettingStudyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JpaSettingStudyApplication.class, args);
 	}
